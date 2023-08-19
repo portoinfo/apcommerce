e2214959b9b64e7696303ee267f86368f33f9300
Adelcio Porto <portoinfo@sercomtel.com.br>
2023-08-19 10:50:36 -0300

    Equals and HashCode

--- src/main/java/com/apcommerce/apcommerce/entities/User.java
+++ src/main/java/com/apcommerce/apcommerce/entities/User.java
@@ -17,6 +17,32 @@ import jakarta.persistence.Table;
 @Table(name = "tb_user")
 public class User {
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
