25975996441243c3b76c84fe80d6d19d4146645e
Adelcio Porto <portoinfo@sercomtel.com.br>
2023-08-20 10:02:19 -0300

    GET all Products with pagination

--- src/main/java/com/apcommerce/apcommerce/controllers/ProductController.java
+++ src/main/java/com/apcommerce/apcommerce/controllers/ProductController.java
@@ -3,6 +3,8 @@ package com.apcommerce.apcommerce.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +24,10 @@ public class ProductController {
 	public ProductDTO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
+
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
+	}
+
 }
